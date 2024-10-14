e1997169b7663dae4b0fd45d3bbe9030d6576fe6
valeriodiste <valeriodstfn@gmail.com>
2024-10-14 20:22:03 +0200

    .

--- ServerModel/server.py
+++ ServerModel/server.py
@@ -1,6 +1,7 @@
 # General libraries
 import os
 import random
+import json
 # Torch libraries
 import torch 
 from torch import nn 
@@ -8,6 +9,9 @@ from torch import nn
 from flask import Flask, request, jsonify
 # Custom libraries
 from fnn import FNN, ModelData
+# Email libraries
+import smtplib
+from email.mime.text import MIMEText
 
 # General parameters
 # REFRESH_DATA = True
@@ -15,8 +19,8 @@ from fnn import FNN, ModelData
 
 # Model data parameters
 NORMALIZE_SENSOR_DATA = True
-NORMALIZATION_RANGE = [-2, 2]
-MAX_WEIGHT = 1000
+NORMALIZATION_RANGE = [-1, 1]
+MAX_WEIGHT = 2000
 MEASUREMENTS = [
 	"accelerometer",
 	# "deviceTemperature",	# Not collected, hence always equal to 0
@@ -32,15 +36,15 @@ MEASUREMENTS = [
 # Model parameters
 FNN_HIDDEN_DIM = 1024
 FNN_HIDDEN_LAYERS = 3
-FNN_ACTIVATION = "ReLU"
+FNN_ACTIVATION = "LeakyReLU"
 FNN_DROPOUT = 0.5
 FNN_OPTIMIZER = "AdamW"
-FNN_LR = 0.00001
+FNN_LR = 0.0000001
 
 # Model training parameters
 DATA_SPLIT = [0.9, 0.075, 0.025]	# Train, validation, test
 BATCH_SIZE = 16
-MAX_EPOCHS = 300
+MAX_EPOCHS = 750
 
 # Flask server initializazion
 app = Flask(__name__)
@@ -131,3 +135,93 @@ def predict():
 	except Exception as e:
 		print("An error occurred:\n" + str(e))
 		return jsonify({"error": str(e)})
+
+# Route to login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	try:
+		# Get the data (data is sent through a GET request as a JSON object request with a certain JSON body, containing the sensor data)
+		data = request.get_json()
+		# Get the "email" field (only the email field is sent to then generate a one time password sent via email to the user)
+		email = data["email"]
+		# Check that the email is in the correct format
+		if "@" not in email or "." not in email:
+			return jsonify({"success": False, "error": "Invalid email format"})
+		# Generate a one time password
+		otp = random.randint(100000, 999999)
+		# Print the OTP
+		print("OTP: " + str(otp))
+		# Store the OTP in the "otp.json" file (containing a JSON objects with the emails as keys and the OTP as values), adding the new OTP to the list
+		otp_file_path = "./mysite/otp.json"
+		otp_file = open(otp_file_path, "r")
+		otp_data = json.load(otp_file)
+		otp_file.close()
+		# Modify the OTP data (check if the email key is already present in the otp data, if it is, modify the OTP, if it is not, add the email and OTP)
+		# otp_data.append({"email": email, "otp": str(otp)})
+		otp_data[email] = str(otp)
+		# Save the OTP data to the file
+		otp_file = open(otp_file_path, "w")
+		json.dump(otp_data, otp_file)
+		otp_file.close()
+		# Send the email with the OTP
+		send_email(email, otp)
+		# Return the response
+		return jsonify({"success": True})
+	except Exception as e:
+		print("An error occurred:\n" + str(e))
+		return jsonify({"success": False, "error": str(e)})
+def send_email(email, otp):
+	# creates SMTP session
+	s = smtplib.SMTP('smtp.gmail.com', 587)
+	# s = smtplib.SMTP('smtp.gmail.com', 465)
+	# start TLS for security
+	s.starttls()
+	# My own email and password
+	my_email = "foodpal.otp@gmail.com"
+	my_password = "kmid sbfs thhs bjus"	# app password
+	# Authentication
+	s.login(my_email, my_password)
+	# message to be sent
+	message_subject = "FoodPal OTP Code"
+	message_body = "Your one time password for FoodPal is:\n\n" + str(otp)
+	message = MIMEText(message_body)
+	message['Subject'] = message_subject
+	message['From'] = my_email
+	message['To'] = email
+	# sending the mail
+	s.sendmail(my_email, email, message.as_string())
+	# terminating the session
+	s.quit()
+
+# Route to verify the OTP
+@app.route('/verify', methods=['GET', 'POST'])
+def verify():
+	try:
+		# Get the data (data is sent through a GET request as a JSON object request with a certain JSON body, containing the sensor data)
+		data = request.get_json()
+		# Get the "email" and "otp" fields (both the email and the OTP are sent to then verify the OTP)
+		email = data["email"]
+		otp = data["otp"]
+		# Verify the OTP
+		otp_file_path = "./mysite/otp.json"
+		otp_file = open(otp_file_path, "r")
+		otp_data = json.load(otp_file)
+		otp_file.close()
+		verified = False
+		if email in otp_data:
+			if otp_data[email] == str(otp):
+				verified = True
+				# Remove the OTP from the file
+				del otp_data[email]
+				otp_file = open(otp_file_path, "w")
+				json.dump(otp_data, otp_file)
+				otp_file.close()
+		# Return the response
+		if not verified:
+			return jsonify({"verified": False, "error": "Invalid OTP"})
+		else:
+			return jsonify({"verified": verified})
+	except Exception as e:
+		print("An error occurred:\n" + str(e))
+		return jsonify({"verified": False, "error": str(e)})
+
